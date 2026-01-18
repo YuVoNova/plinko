7a2028f004ea4a5e433a718bed3370f63fdd8c3e
Yusuf Metindoğan <yusufmdogan@gmail.com>
2026-01-18 17:38:26 +0300

    feat: plinko implementation
    - Initial Plinko gameplay implementation with TestManager (without MockServer)
    - Import TextMeshPro
    - ObjectPool implementation

---
+++ Plinko/Assets/Scripts/Core/IPoolable.cs
@@ -0,0 +1,11 @@
+﻿namespace Core
+{
+    /// <summary>
+    /// Interface for poolable objects
+    /// </summary>
+    public interface IPoolable
+    {
+        public void OnSpawn();
+        public void OnDespawn();
+    }
+}
---
+++ Plinko/Assets/Scripts/Data/LevelConfig.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Data
+{
+    [Serializable]
+    public class LevelConfig
+    {
+        public int levelID;
+        public float[] bucketMultipliers;
+        
+        public LevelConfig(int levelID, float[] bucketMultipliers)
+        {
+            this.levelID = levelID;
+            this.bucketMultipliers = bucketMultipliers;
+        }
+    }
+}
